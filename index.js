5c28c2c2e5273e06dd015414bd3ccf1f9bcbedcf
Antony Caseres <accasere@outlook.com>
2022-05-08 01:09:49 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+
+//Adding EventListener to the key
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+  });
+
+//Function for move object to the left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+//Function for move object to the right side
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360){
+        console.log(dodger.style.left = `${left + 1}px`)
+    }
+}
